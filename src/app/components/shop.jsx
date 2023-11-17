d5090d2c2af71db095fc2da0affaa8309881f7fc
y.kono <y.kono@wakuto.net>
2023-11-17 14:46:42 +0900

    homepage

---
+++ src/app/components/shop.jsx
@@ -0,0 +1,43 @@
+"use client";
+import React, { useEffect, useState } from 'react'
+import "./shop.css";
+
+function Shop() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        setItems(json);
+      })
+  }, [])
+
+  return (
+    <div className="shop">
+    <div className="shopTitle">
+      <h1>E-commerce Shop</h1>
+    </div>
+
+    <div className="products">
+      {items.map((item) => (
+         <div key={item.id} className="product">
+           <div className="content">
+             <p className="title">
+               {item.id}. {item.title}
+             </p>
+             <img src={item.image} alt={item.title} className="image" />
+             <p className="price">${item.price}</p>
+             <p className="description">{item.description}</p>
+             <p className="category">{item.category}</p>
+           </div>
+           <button className="addToCartBtn">ADD</button>
+        </div>
+      ))}
+    </div>
+  </div>
+  );
+}
+
+export default Shop
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Shop from './components/shop'
+
 
 export default function Home() {
   return (
-    <div>test</div>
+    <div>
+      <Shop/>
+    </div>
   )
 }
